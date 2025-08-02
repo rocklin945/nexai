0fe41874d6404c305037b8c0fd79cbfde9c7fbe3
Rock Lin <2397812547@qq.com>
2025-08-02 09:38:19 +0800

    init

---
+++ src/main/java/com/rocklin/nexai/NexaiApplication.java
@@ -0,0 +1,13 @@
+package com.rocklin.nexai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rocklin/nexai/NexaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocklin.nexai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
